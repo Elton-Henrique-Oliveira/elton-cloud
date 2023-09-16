1d8d422776a59a89c045b6702b6e5f40c914f992
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-09-16 19:03:26 -0300

    teste 1

---
+++ config-server/src/test/java/br/com/elton/springservice/SpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elton.springservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-service/src/main/java/br/com/elton/springservice/configuration/GreetingConfiguration.java
@@ -0,0 +1,2 @@
+package br.com.elton.springservice.configuration;public class GreetingConfiguration {
+}
